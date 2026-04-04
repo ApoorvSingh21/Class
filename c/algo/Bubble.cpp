169a725e6ea605d43829dbb118624fa8f5281007
Apoorv Singh <avsinghrathorft@gmail.com>
2026-04-04 16:18:45 +0530

    t_bubble

--- c/algo/Bubble.cpp
+++ c/algo/Bubble.cpp
@@ -1,4 +1,19 @@
+/*  1. Bubble Sort
+Theory:
+Bubble Sort is a simple comparison-based sorting algorithm. It repeatedly steps through the 
+list, compares adjacent elements, and swaps them if they are in the wrong order. This 
+process is repeated until the list is sorted.
+It is called Bubble Sort because smaller elements “bubble” to the top of the list, and larger 
+elements sink to the bottom.
+Algorithm:
+1. Start from the first element, compare it with the next element.
+2. If the current element is greater than the next element, swap them.
+3. Move to the next element and repeat the process for all elements.
+4. Continue the process for all elements until no swaps are needed, meaning the list is 
+sorted. */
+
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 void Bubble_Sort(int s_arr[],int len){
