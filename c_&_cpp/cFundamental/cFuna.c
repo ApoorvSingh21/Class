7bf1011f9cfe05179fc58e2413fffd9d9e4cce05
Apoorv Singh <avsinghrathorft@gmail.com>
2026-04-06 22:12:32 +0530

    new Structure

---
+++ c_&_cpp/cFundamental/cFuna.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){int a,b ;
+    printf("hello would\n");
+    a = 1;
+    b = 2;
+printf("a + b = %d\n",a+b);
+printf("a - b = %d\n",a-b);
+printf("a * b = %d\n",a*b);
+printf("a / b = %d\n",a/b);
+printf("a % b = %d\n",a%b);
+printf("a++ = %d\n",a++);
+printf("++a = %d\n",++a);
+printf("\n");
+
+return 0;}
