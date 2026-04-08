68215f12534c40bf48caea1b3c41109d3f86e254
Apoorv Singh <avsinghrathorft@gmail.com>
2026-04-09 05:03:10 +0530

    java PY

--- 2JAVA/addTwoNo.java
+++ 2JAVA/addTwoNo.java
@@ -1,14 +1,26 @@
 import java.util.*;
-public class addTwoNo{
+
+ class Two {
   public static int add(int a,int b) {
     return a+b;
   }
+  
+}
+
+
+public class addTwoNo{
+  // public static int add(int a,int b) {
+    // return a+b;
+  // }
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    // two to = new two();
+
     System.out.println("Enter two integer value you want to add :");
     int a = scan.nextInt();
     int b = scan.nextInt();
-    System.out.println( "sum of given value is : "+add(a,b));
+
+    System.out.println( "sum of given value is : "+Two.add(a,b));
 
   }
 }
