efc532d2ba57d23aeb18da7a84c19ccddafbd63a
Apoorv <avsinghrathorft@gmail.com>
2026-05-19 17:31:29 +0530

    stackSTL

---
+++ 5DataStructure/stackSTL.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<int>s;
+    s.push(1);
+   cout<< s.size()<<"\n";
+   s.pop();
+   cout<< s.size();
+
+
+return 0;}
